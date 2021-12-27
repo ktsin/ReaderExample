e58f5af9a47d2b3790df3335e369cfc2ad53fba6
User <User@LXIBY1060>
2021-12-27 17:41:13 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Reader|Writer example");
+
+            var file = new Reader("contextExample.txt");
+
+            var example = "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+            file.SaveContent(example);
+
+            Console.WriteLine($"Is result is equal to source?\n {String.Equals(example, file.GetContent())}");
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Reader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Reader
+    {
+        public Reader(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; set; }
+
+        public string GetContent()
+        {
+            string result = "";
+            try
+            {
+                if (File.Exists(Path))
+                {
+                    using StreamReader stream = File.OpenText(Path);
+                    result = stream.ReadToEnd();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+            return result;
+        }
+
+        public void SaveContent(string message)
+        {
+            try
+            {
+                using FileStream fileStream = File.Open(Path, FileMode.OpenOrCreate, FileAccess.Write);
+                if (fileStream.CanWrite)
+                {
+                    using StreamWriter writer = new StreamWriter(fileStream);
+                    writer.Write(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+    }
+}
